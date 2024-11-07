831d1c38da109f3e422371fc6516e25be0798fb7
webinfomatrix <brijmohanwebinfomatrix@gmail.com>
2024-11-07 10:42:52 +0530

    first commit

---
+++ controler/user.controler.js
@@ -0,0 +1,87 @@
+// Import section
+require('dotenv').config();
+const Joi = require('joi')
+const joiToForms = require('joi-errors-for-forms').form
+const jwt = require('jsonwebtoken')
+const md5 = require('md5')
+
+
+module.exports.login = async function (req, res) {
+
+    try {
+        const bodyData = req.body
+        // Validation part 
+        const validatedObject = Joi.object({
+
+            email: Joi.string().email().insensitive().lowercase().required(),
+            password: Joi.string().min(5).max(16).required(),
+        })
+        /* validating the validation values */
+        const validateValue = validatedObject.validate({
+            email: bodyData.email,
+            password: bodyData.password
+        }, {
+            abortEarly: false
+        })
+
+        /* converts errors in key : value pair */
+        const convertToForms = joiToForms()
+
+        const validationError = convertToForms(validateValue.error)
+
+        if (validationError) {
+
+            return res.status(200).json({
+                success: false,
+                message: "validation error"
+            })
+        }
+
+        const validatedValues = changeCaseObject.snakeCase(validateValue.value)
+
+        // find user by email
+
+        const findUserOptions = {
+            raw: true,
+            attributes: ['email'],
+            where: {
+                email: validatedValues.email,
+                password: md5(validatedValues.password)
+            }
+        }
+        try {
+            models.User.findAll(findUserOptions).then(data => {
+                if (data.length > 0) {
+                    const userPayload = {
+                        email: data[0].email,
+                        userId: data[0].id,
+                        role: data[0].user_type
+                    }
+                    const aToken = jwt.sign(
+                        userPayload,
+                        process.env.JWT_SECRET_KEY,
+                        {
+                            subject: 'access',
+                            expiresIn: '7d'
+                        }
+                    )
+                    res.status(200).json({
+                        success: true,
+                        message: 'login successfull',
+                        response: aToken
+                    })
+                }
+            })
+        } catch (error) {
+            res.status(401).json({
+                success: false,
+                message: `database error: ${error}`
+            })
+        }
+    } catch (error) {
+        res.status(500).json({
+            success: false,
+            message: error
+        })
+    }
+}
